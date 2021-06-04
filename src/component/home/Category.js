f7c965a0af797c6bf7b46a5d87f2b59e8c61ffe3
@yopigambyok <yopigambyok@gmail.com>
2021-06-05 06:21:34 +0700

    location start

--- src/component/home/Category.js
+++ src/component/home/Category.js
@@ -1,7 +1,77 @@
+
+import example from './../../assets/images/example.jpg';
+import cinema from './../../assets/images/cinema.jpg';
+import explore from './../../assets/images/explore-place.jpg';
+import fitnes from './../../assets/images/fitnes.jpg';
+import history from './../../assets/images/history.jpg';
+import nightlife from './../../assets/images/nightlife.jpg';
+import restaurant from './../../assets/images/restaurant.jpg';
+import event from './../../assets/images/event.jpg';
+import outdoor from './../../assets/images/outdoor.jpg';
+
 export default function Category() {
     return(
-        <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit voluptate excepturi molestias velit, error quibusdam adipisci quos quia! Accusantium illum fugit repellat dolor. Similique doloribus consequuntur minima molestias doloremque ut!
+        <div className="category container">
+            <div className="content">
+                <div className="content-head">
+                    <h2>Ontdek kinderopvang in verschillende steden</h2>
+                    <p>Filter by category</p>
+                </div>
+                <div className="content-body">
+                    <div className="box box-6">
+                        <div className="bg bg-purple">
+                            <h4>Explore places</h4>
+                        </div>
+                        <img src={explore} alt="" />
+                    </div>
+                    <div className="box box-6">
+                        <div className="bg">
+                            <h4>Nightlife</h4>
+                        </div>
+                        <img src={nightlife} alt="" />
+                    </div>
+                    <div className="box box-3">
+                        <div className="bg">
+                            <h4>Cinema</h4>
+                        </div>
+                        <img src={cinema} alt="" />
+                    </div>
+                    <div className="box box-3">
+                        <div className="bg">
+                            <h4>Restaurant</h4>
+                        </div>
+                        <img src={restaurant} alt="" />
+                    </div>
+                    <div className="box box-3">
+                        <div className="bg">
+                            <h4>Gym & Fitness</h4>
+                        </div>
+                        <img src={fitnes} alt="" />
+                    </div>
+                    <div className="box box-3">
+                        <div className="bg">
+                            <h4>Art & History</h4>
+                        </div>
+                        <img src={history} alt="" />
+                    </div>
+                    <div className="box box-3">
+                        <div className="bg">
+                            <h4>Outdoor Activities</h4>
+                        </div>
+                        <img src={outdoor} alt="" />
+                    </div>
+                    <div className="box box-3 box-text">
+                        <p>To see a World a Grain of Sand And a Heaven in a Wild Flower Hold Infinity in the palm of your hand And Eternity in an hour</p>
+                        <h4>William Blake</h4>
+                    </div>
+                    <div className="box box-6">
+                        <div className="bg bg-purple">
+                            <h4>Discover Events</h4>
+                        </div>
+                        <img src={event} alt="" />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/component/home/Location.js
+++ src/component/home/Location.js
@@ -1,7 +1,52 @@
+import { FiBookmark, FiDollarSign, FiHeart, FiMapPin, FiZoomIn } from 'react-icons/fi';
+import example from './../../assets/images/example.jpg';
+
 export default function Location() {
     return(
-        <div>
-            
+        <div className="location container">
+            <div className="content">
+                <div className="content-head">
+                    <h2>Ontdek kinderopvang in verschillende steden</h2>
+                    <p>Filter by category</p>
+                </div>
+                <div className="content-body">
+                    <div className="card-product">
+                        <div className="card-top">
+                            <div className="bg">
+                                <div className="box-icon">
+                                    <FiDollarSign />
+                                </div>
+                            </div>
+                            <img src={example} alt="" />
+                        </div>
+                        <div className="card-bottom">
+                            <div className="title">
+                                <h4>VGO - Boekel - Bexkens</h4>
+                                <div className="title-info">
+                                    <FiMapPin />
+                                    <h5>Meewenlaan 36</h5>
+                                </div>
+                            </div>
+                            <div className="card-footer">
+                                <div className="type-card">
+                                    <div className="circle">
+                                        <FiBookmark />
+                                        <h4>VGO</h4>
+                                    </div>
+                                </div>
+                                <div className="action-card">
+                                    <div className="circle">
+                                        <FiZoomIn />
+                                    </div>
+                                    <div className="circle">
+                                        <FiHeart />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
